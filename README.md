6b5904f4661aa264d1072c1c6f97e0f2f748a3e7
rlipi <renatolipi@gmail.com>
2017-05-21 01:08:50 -0300

    WIP: add initial webservice

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # webcrawler_experiment
 temporary experiment; this should be replaced by another repo(s)
+
+-----
+
+#### How to run?
+
+  Using your terminal, run:
+
+  `python app.py`
+
+  Then go to your web browser and try:
+
+  _http://127.0.0.1:5000/_
+
+  and then:
+
+  _http://127.0.0.1:5000/feed/autoesporte/_
---
+++ app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify, abort
+
+from webcrawlers import AutoEsporteWC
+
+
+app = Flask(__name__)
+
+
+JSON_RESPONSE_HEADERS = {'Content-Type': 'application/json'}
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'It works! (health check)', 200
+
+
+@app.route('/feed/autoesporte/', methods=['GET'])
+def autoesporte():
+    autoespoete_wc = AutoEsporteWC()
+
+    response = autoespoete_wc.get_json_content()
+    if response:
+        return jsonify(response), 200, JSON_RESPONSE_HEADERS
+
+    abort(404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
