be209dc830b66dcaa62dd172df7de0c28a08b22e
rlipi <renatolipi@gmail.com>
2017-05-21 00:39:55 -0300

    WIP: parsing content

--- webcrawlers/autoesporte.py
+++ webcrawlers/autoesporte.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup as BS
 import requests
 
 
@@ -15,6 +16,32 @@ class AutoEsporteWC(object):
 
         return None
 
-    def get_json_content(self):
+    def _get_content_description(self, descricao):
+        description = []
+        return description
 
-        return self._get_content()
+    def _jsonify_content(self, content):
+        feed_list = []
+
+        parsed_content = BS(content, "html.parser")
+        items = parsed_content.find_all('item')
+
+        for item in items:
+            descricao = BS(item.description.string, "html.parser")
+            json_item = {
+                'title': item.title.string or '',
+                'link': item.link.string or '',
+                'description': self._get_content_description(descricao)
+            }
+            feed_list.append({'item': json_item})
+
+        json_content = {'feed': feed_list}
+
+        return json_content
+
+    def get_json_content(self):
+        content = self._get_content() or {}
+        if content:
+            content = self._jsonify_content(content)
+
+        return content
