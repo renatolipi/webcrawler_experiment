cac02f6c5adeb2b504e5ba80c4cc63cb6bf31232
rlipi <renatolipi@gmail.com>
2017-05-21 00:29:18 -0300

    WIP: expose crawler class; get raw content from feed

---
+++ webcrawlers/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from autoesporte import AutoEsporteWC
--- webcrawlers/autoesporte.py
+++ webcrawlers/autoesporte.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import requests
+
 
 class AutoEsporteWC(object):
 
     URL = 'http://revistaautoesporte.globo.com/rss/ultimas/feed.xml'
 
-    def get_content(self):
-        return {}
+    def _get_content(self):
+        response = requests.get(self.URL)
+
+        if response.status_code == 200:
+            return response.content
+
+        return None
+
+    def get_json_content(self):
+
+        return self._get_content()
